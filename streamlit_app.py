fa184c101f10964d466a79553f0131f6e7af3350
vixuowis <vixuowis@users.noreply.github.com>
2025-09-03 12:10:02 +0000

    更新页面路径格式，优化导航结构

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,11 @@ import streamlit as st
 
 pg = st.navigation([
     st.Page("pages/0_home.py", title="首页", icon="🏠"),
-    st.Page("pages/1_union_wrap.py", title="联合封装", icon="🔗"),
-    st.Page("pages/2_realtime_wrap.py", title="实时封装", icon="📈"),
-    st.Page("pages/3_safe_release.py", title="可控发布", icon="✅"),
+    st.Page("pages/a_union_wrap.py", title="联合封装", icon="🔗"),
+    st.Page("pages/b_realtime_wrap.py", title="实时封装", icon="📈"),
+    st.Page("pages/c_safe_release.py", title="可控发布", icon="✅"),
 ])
+
+st.sidebar.selectbox("## 场景选择", ["地质灾害", "海洋牧场", "高原脆弱", "农业智慧", "森林生态"], key="data_scene")
 
 pg.run()
