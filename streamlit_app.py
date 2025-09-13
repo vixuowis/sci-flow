0db4781426346d9e5edfac1d674874d21bdef979
Songyang Gao <93965787+songyang0603@users.noreply.github.com>
2025-09-13 13:45:52 +0000

    updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,6 @@ pg = st.navigation([
     st.Page("pages/a_union_wrap.py", title="联合封装", icon="🔗"),
     st.Page("pages/b_realtime_wrap.py", title="实时封装", icon="📈"),
     st.Page("pages/c_safe_release.py", title="可控发布", icon="✅"),
-    st.Page("pages/4_test.py", title="test", icon="✅"),
 ])
 
 st.sidebar.selectbox("## 场景选择", ["地质灾害", "海洋牧场", "高原脆弱", "农业智慧", "森林生态"], key="data_scene")
